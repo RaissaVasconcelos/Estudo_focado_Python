e69928c4222706331f703611f6478760060bcb6b
Raissa <76914915+RaissaVasconcelos@users.noreply.github.com>
2021-04-25 23:35:31 -0400

    Add files via upload

---
+++ Diferenca.py
@@ -0,0 +1,21 @@
+#Uri 1007
+
+def diferenca(result_1,result_2):
+    return result_1 - result_2
+
+
+if __name__ == '__main__':   
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+    result_1 = a*b
+    result_2 = c*d
+    resultado = diferenca(result_1,result_2)
+    print(f'DIFERENCA = {resultado}')
+
+
+    
+
+    
+
